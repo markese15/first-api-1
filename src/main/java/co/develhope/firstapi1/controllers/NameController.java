9b45336193adc71220b68c8e853b8da32510ad7b
mirko <mirkmarkese@gmail.com>
2023-03-07 13:03:17 +0100

    Initial commit

---
+++ src/main/java/co/develhope/firstapi1/controllers/NameController.java
@@ -0,0 +1,20 @@
+package co.develhope.firstapi1.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NameController {
+    @GetMapping (value= "/name")
+    public String getName(){
+        return "mirko"; }
+    @PostMapping (value= "/name")
+    public StringBuilder postName(StringBuilder name){
+        name= new StringBuilder(new StringBuilder("mirko").reverse().toString());
+
+        return name ;
+    }
+
+}
